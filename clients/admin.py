3edb2da565cc7da697cafdc3f1be9c78897163c4
Vitalii Husarev <shagode@ukr.net>
2021-02-21 19:05:41 +0200

    Created phone and email models for the company. Installed factory_boy module. Tests are now a package. Created validator for the phone field.

--- clients/admin.py
+++ clients/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from clients.models import Company
+from clients.models import Phone, Email, Company
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(Phone)
+admin.site.register(Email)
--- clients/models.py
+++ clients/models.py
@@ -2,6 +2,39 @@ from django.db import models
 
 from tinymce.models import HTMLField
 
+from clients.validators import PhoneValidator
+
+
+class Phone(models.Model):
+    """Client phone model."""
+    class Meta:
+        verbose_name = "телефон"
+        verbose_name_plural = "телефоны"
+
+    owner = models.CharField(verbose_name="владелец", max_length=150)
+    number = models.CharField(
+        verbose_name="номер",
+        max_length=15,
+        unique=True,
+        validators=[PhoneValidator]
+    )
+
+    def __str__(self):
+        return f"{self.owner} <+{self.number}>"
+
+
+class Email(models.Model):
+    """Client email model."""
+    class Meta:
+        verbose_name = "email"
+        verbose_name_plural = "emails"
+
+    owner = models.CharField(verbose_name="владелец", max_length=150)
+    address = models.EmailField(verbose_name="адрес электронной почты", unique=True)
+
+    def __str__(self):
+        return f"{self.owner} <{self.address}>"
+
 
 class Company(models.Model):
     """Client company model."""
@@ -9,12 +42,14 @@ class Company(models.Model):
         verbose_name = "компания"
         verbose_name_plural = "компании"
 
-    name = models.CharField(verbose_name="название", max_length=150, blank=False)
-    contact_person = models.CharField(verbose_name="контактное лицо", max_length=150, blank=False)
-    description = HTMLField(verbose_name="описание", blank=False)
-    address = models.CharField(verbose_name="адрес", max_length=200, blank=False)
+    name = models.CharField(verbose_name="название", max_length=150, unique=True)
+    contact_person = models.CharField(verbose_name="контактное лицо", max_length=150)
+    description = HTMLField(verbose_name="описание")
+    address = models.CharField(verbose_name="адрес", max_length=200)
     created = models.DateTimeField(verbose_name="дата создания записи", auto_now_add=True)
     updated = models.DateTimeField(verbose_name="дата изменения записи", auto_now=True)
+    phones = models.ManyToManyField(Phone, verbose_name="phones")
+    emails = models.ManyToManyField(Email, verbose_name="emails")
 
     def __str__(self):
         return self.name
