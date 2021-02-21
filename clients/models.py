697f2aca87894a9180e45cbd312b5da10892fb8c
Vitalii Husarev <shagode@ukr.net>
2021-02-21 13:58:11 +0200

    Startapp "Clients" and created a basic structure for the company model.

---
+++ clients/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Company(models.Model):
+    """Client company model."""
+    class Meta:
+        verbose_name = "компания"
+        verbose_name_plural = "компании"
+
+    name = models.CharField(verbose_name="название", max_length=150, blank=False)
+    contact_person = models.CharField(verbose_name="контактное лицо", max_length=150, blank=False)
+    description = models.TextField(verbose_name="описание")
+    address = models.CharField(verbose_name="адрес", max_length=200, blank=False)
+    created = models.DateTimeField(verbose_name="дата создания записи", auto_now_add=True)
+    updated = models.DateTimeField(verbose_name="дата изменения записи", auto_now=True)
+
+    def __str__(self):
+        return self.name
