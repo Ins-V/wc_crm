f1e599c4606d87e88c46ee7d6991f40df52cd715
Vitalii Husarev <shagode@ukr.net>
2021-02-24 14:55:21 +0200

    Made display settings in the admin panel for the company model.

--- clients/admin.py
+++ clients/admin.py
@@ -5,7 +5,21 @@ from clients.models import Phone, Email, Company
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['name']
+    readonly_fields = ('created', 'updated')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description')
+        }),
+        ('Контакты', {
+            'fields': ('contact_person', 'address', 'phones', 'emails'),
+        }),
+        ('Даты', {
+            'fields': ('created', 'updated',),
+        }),
+    )
+    filter_horizontal = ('phones', 'emails')
+    save_on_top = True
 
 
 admin.site.register(Phone)
